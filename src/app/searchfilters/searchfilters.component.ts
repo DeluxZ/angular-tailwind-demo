c080b816e172a5c53c552830fc846ba373fefa94
Harrie Essing <harrie.essing@orangenxt.com>
2019-07-16 20:43:23 +0200

    Fix ng build

--- src/app/searchfilters/searchfilters.component.ts
+++ src/app/searchfilters/searchfilters.component.ts
@@ -8,7 +8,7 @@ import { Globals } from '../globals';
 })
 export class SearchfiltersComponent implements OnInit {
 
-  constructor(private globals: Globals) { }
+  constructor(public globals: Globals) { }
 
   ngOnInit() {
   }
--- src/app/siteheader/siteheader.component.ts
+++ src/app/siteheader/siteheader.component.ts
@@ -8,7 +8,7 @@ import { Globals } from '../globals';
 })
 export class SiteheaderComponent implements OnInit {
 
-  constructor(private globals: Globals) { }
+  constructor(public globals: Globals) { }
 
   ngOnInit() {
   }
