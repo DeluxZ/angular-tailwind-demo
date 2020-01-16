68716a6db9277976e2e92ceb9f1e549701e0cfc0
Harrie Essing <harrie.essing@ict.nl>
2020-01-16 21:08:28 +0100

    Dockerizing with hot reload

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to angular-tailwind-demo!');
+    expect(page.getTitleText()).toEqual('Welcome to workcation-angular!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-tailwind-demo'`, () => {
+  it(`should have as title 'workcation-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-tailwind-demo');
+    expect(app.title).toEqual('workcation-angular');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-tailwind-demo!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to workcation-angular!');
   });
 });
