021c1072817cfb2ef5079b73788215590fe6932d
Harrie Essing <harrie.essing@gmail.com>
2021-09-28 13:42:54 +0200

    Remove import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,13 @@ import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SplashpageComponent }  from './splashpage/splashpage.component';
 import { DashboardComponent }   from './dashboard/dashboard.component';
-import { MaterialDesignComponent } from './material-design/material-design.component';
 
 const routes: Routes = [
   { path: 'welcome', redirectTo: '/', pathMatch: 'full' },
   { path: 'splashpage', redirectTo: '/', pathMatch: 'full' },
   { path: '', component: SplashpageComponent },
   { path: 'dashboard', redirectTo: '/app', pathMatch: 'full' },
-  { path: 'app', component: DashboardComponent },
-  { path: 'mdcolors', component: MaterialDesignComponent }
+  { path: 'app', component: DashboardComponent }
 ];
 
 @NgModule({
